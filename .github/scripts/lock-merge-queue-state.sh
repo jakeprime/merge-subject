e10fe7be288ebfd2a455318ee8ad46bbe49b36ab
Jake Prime <jake@jakeprime.com>
2025-05-18 11:41:47 +0100

    Start separating out actions
    First up is locking

---
+++ .github/scripts/lock-merge-queue-state.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+for i in {1..10}; do
+  git fetch origin merge-queue-state --depth 1
+  if git ls-tree --name-only origin/merge-queue-state lock | grep -q 'lock'; then
+    echo "State is locked"
+  else
+    git checkout merge-queue-state
+    echo "$GITHUB_RUN_ID" > lock
+    git add lock
+    git commit -m "Locking merge queue state"
+
+    # this will fail if another runner has pushed a lock in the meantime
+    if git push; then
+      exit 0
+    else
+      git reset --hard HEAD~1
+    fi
+  fi
+
+  sleep 5
+done
+
+echo "Failed to get lock on merge state"
+exit 1
---
+++ .github/scripts/unlock-merge-queue-state.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+git checkout merge-queue-state
+rm lock
+git add lock
+git commit -m "Unlocking merge queue state"
+git push
